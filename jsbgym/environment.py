308ed96d21b31ecc5de316966548d5ea56ff973c
sryu1 <95025816+sryu1@users.noreply.github.com>
2023-01-01 20:15:51 +1000

    Converted to gymnasium?

--- jsbgym/environment.py
+++ jsbgym/environment.py
@@ -10,15 +10,15 @@ from typing import Optional, Type, Tuple, Dict
 class JsbSimEnv(gym.Env):
     """
     A class wrapping the JSBSim flight dynamics module (FDM) for simulating
-    aircraft as an RL environment conforming to the OpenAI Gym Env
+    aircraft as an RL environment conforming to the Gymnasium Env
     interface.
 
     An JsbSimEnv is instantiated with a Task that implements a specific
     aircraft control task with its own specific observation/action space and
     variables and agent_reward calculation.
 
-    ATTRIBUTION: this class implements the OpenAI Gym Env API. Method
-    docstrings have been adapted or copied from the OpenAI Gym source code.
+    ATTRIBUTION: this class implements the Gymnasium Env API. Method
+    docstrings have been adapted or copied from the Gymnasium source code.
     """
 
     JSBSIM_DT_HZ: int = 60  # JSBSim integration frequency
@@ -81,106 +81,56 @@ class JsbSimEnv(gym.Env):
         if action.shape != self.action_space.shape:
             raise ValueError("mismatch between action and action space size")
 
-        state, reward, done, info = self.task.task_step(
-            self.sim, action, self.sim_steps_per_agent_step
-        )
-        return np.array(state), reward, done, info
+        state, reward, done, info = self.task.step(self.sim, action)
 
-    def reset(self):
+        for _ in range(self.sim_steps_per_agent_step):
+            self.sim.run_one_step()
+
+        return state, reward, done, info
+
+    def reset(self, seed: Optional[int] = None) -> np.ndarray:
         """
-        Resets the state of the environment and returns an initial observation.
+        Reset the state of the environment and returns an initial observation.
 
-        :return: array, the initial observation of the space.
+        :param seed: int, seed for the random number generator
         """
-        init_conditions = self.task.get_initial_conditions()
-        if self.sim:
-            self.sim.reinitialise(init_conditions)
-        else:
-            self.sim = self._init_new_sim(
-                self.JSBSIM_DT_HZ, self.aircraft, init_conditions
-            )
-
-        state = self.task.observe_first_state(self.sim)
-
-        if self.flightgear_visualiser:
-            self.flightgear_visualiser.configure_simulation_output(self.sim)
-        info = {}
-        if self.render_mode == "human":
-            self.render()
-        return np.array(state), info
-
-    def _init_new_sim(self, dt, aircraft, initial_conditions):
-        return Simulation(
-            sim_frequency_hz=dt, aircraft=aircraft, init_conditions=initial_conditions
+        dt = 1 / self.JSBSIM_DT_HZ
+        initial_conditions = self.task.get_initial_conditions()
+        self.sim = self._init_new_sim(dt, self.aircraft, initial_conditions, seed)
+        self.figure_visualiser = FigureVisualiser(
+            dt=dt, aircraft=self.aircraft, sim=self.sim
         )
+        self.flightgear_visualiser = FlightGearVisualiser(
+            dt=dt, aircraft=self.aircraft, sim=self.sim
+        )
+        return self.task.get_initial_state(self.sim)
 
     def render(self, flightgear_blocking=True):
         mode = self.render_mode
-        """Renders the environment.
-        The set of supported modes varies per environment. (And some
-        environments do not support rendering at all.) By convention,
-        if mode is:
-        - human: render to the current display or terminal and
-          return nothing. Usually for human consumption.
-        - rgb_array: Return an numpy.ndarray with shape (x, y, 3),
-          representing RGB values for an x-by-y pixel image, suitable
-          for turning into a video.
-        - ansi: Return a string (str) or StringIO.StringIO containing a
-          terminal-style text representation. The text can include newlines
-          and ANSI escape sequences (e.g. for colors).
-        Note:
-            Make sure that your class's metadata 'render.modes' key includes
-              the list of supported modes. It's recommended to call super()
-              in implementations to use the functionality of this method.
-
-        :param mode: str, the mode to render with
-        :param flightgear_blocking: waits for FlightGear to load before
-            returning if True, else returns immediately
-        """
-        if mode == "human":
-            if not self.figure_visualiser:
-                self.figure_visualiser = FigureVisualiser(
-                    self.sim, self.task.get_props_to_output()
-                )
-            self.figure_visualiser.plot(self.sim)
-        elif mode == "flightgear":
-            if not self.flightgear_visualiser:
-                self.flightgear_visualiser = FlightGearVisualiser(
-                    self.sim, self.task.get_props_to_output(), flightgear_blocking
-                )
-            self.flightgear_visualiser.plot(self.sim)
-        else:
-            super().render()
+        if mode == "flightgear":
+            self.flightgear_visualiser.run_one_step(blocking=flightgear_blocking)
+        elif mode == "human":
+            self.figure_visualiser.run_one_step()
 
     def close(self):
-        """Cleans up this environment's objects
-
-        Environments automatically close() when garbage collected or when the
-        program exits.
-        """
-        if self.sim:
-            self.sim.close()
-        if self.figure_visualiser:
-            self.figure_visualiser.close()
-        if self.flightgear_visualiser:
+        if self.render_mode == "flightgear":
             self.flightgear_visualiser.close()
+        elif self.render_mode == "human":
+            self.figure_visualiser.close()
 
-    def seed(self, seed=None):
-        """
-        Sets the seed for this env's random number generator(s).
-        Note:
-            Some environments use multiple pseudorandom number generators.
-            We want to capture all such seeds used in order to ensure that
-            there aren't accidental correlations between multiple generators.
-        Returns:
-            list<bigint>: Returns the list of seeds used in this env's random
-              number generators. The first value in the list should be the
-              "main" seed, or the value which a reproducer should pass to
-              'seed'. Often, the main seed equals the provided 'seed', but
-              this won't be true if seed=None, for example.
-        """
-        gym.logger.warn("Could not seed environment %s", self)
-        return
+    def _init_new_sim(
+        self,
+        dt: float,
+        aircraft: Aircraft,
+        initial_conditions: Dict,
+        seed: Optional[int] = None,
+    ):
+        return Simulation(
+            sim_frequency_hz=dt,
+            aircraft=aircraft,
+            init_conditions=initial_conditions,
+            seed=seed,
+        )
 
 
 class NoFGJsbSimEnv(JsbSimEnv):
@@ -195,12 +145,19 @@ class NoFGJsbSimEnv(JsbSimEnv):
 
     metadata = {"render_modes": ["human"], "render_fps": 30}
 
-    def _init_new_sim(self, dt: float, aircraft: Aircraft, initial_conditions: Dict):
+    def _init_new_sim(
+        self,
+        dt: float,
+        aircraft: Aircraft,
+        initial_conditions: Dict,
+        seed: Optional[int] = None,
+    ):
         return Simulation(
             sim_frequency_hz=dt,
             aircraft=aircraft,
             init_conditions=initial_conditions,
             allow_flightgear_output=False,
+            seed=seed,
         )
 
     def render(self, flightgear_blocking=True):
