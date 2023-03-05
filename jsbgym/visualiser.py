532a0cf4cf32640f36234f89082cef5b70e741bd
sryu1 <95025816+sryu1@users.noreply.github.com>
2023-03-05 13:09:52 +1000

    Possble fix to render human error

--- jsbgym/visualiser.py
+++ jsbgym/visualiser.py
@@ -61,7 +61,7 @@ class FigureVisualiser(object):
         for subplot in self.axes[1:]:
             # pop and translate all data points
             while subplot.lines:
-                data = subplot.lines.pop()
+                data = list(subplot.lines).pop()
                 del data
 
         self._print_state(sim)
